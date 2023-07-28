b7b8b6a777376fa8a3fd7dcc8a7cff5894e634e8
joaopedrobyju <joaopedrosesi80@gmail.com>
2023-07-28 10:58:11 -0300

    Declarando Strin com Objeto

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -11,11 +11,14 @@ public class Fundamentos {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Delclaração de variáveis
+        // Delclaração de variáveis de tipos primitivos
         String nome = "Moisés";
         int idade = 40;
         boolean estudando = true;
         double media = 5.5;
+        
+        // 
+        String bairro = new String("Centro");
         
     }
     
