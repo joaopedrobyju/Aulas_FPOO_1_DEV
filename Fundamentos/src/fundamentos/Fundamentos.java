0675b55fcd5e84c76c5863cb7ccbec1b375f5396
joaopedrobyju <joaopedrosesi80@gmail.com>
2023-07-28 10:48:55 -0300

    Criando variáveis em Java

--- Fundamentos/src/fundamentos/Fundamentos.java
+++ Fundamentos/src/fundamentos/Fundamentos.java
@@ -4,17 +4,19 @@
  */
 package fundamentos;
 
-/**
- *
- * @author Aluno
- */
+
 public class Fundamentos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Delclaração de variáveis
+        String nome = "Moisés";
+        int idade = 40;
+        boolean estudando = true;
+        double media = 5.5;
+        
     }
     
 }
