e5a49e46169f0837f96814494d4496d985633240
joaopedrobyju <joaopedrosesi80@gmail.com>
2023-08-31 10:23:27 -0300

    Criando interfaces para validar nome e email do cliente

--- Fundamentos/src/orientacaoobjetos/Locadora.java
+++ Fundamentos/src/orientacaoobjetos/Locadora.java
@@ -15,17 +15,22 @@ public class Locadora {
     public static void main(String[] args) throws UnsupportedEncodingException {
         System.setOut(new PrintStream(System.out, true, "UTF-8"));
         
-        Cliente c1 = new Cliente("João", "joao@gmail.com");
+        Cliente c1 = new Cliente("Ari Santos", "joao@gmail.com");
         System.out.println(c1);
+        System.out.println("-------------------------------------------");
         double valorLocacaoSimples = c1.CalcularLocacao(3);
         System.out.println("O valor para locação do filme " + "Simples é: " + valorLocacaoSimples);
+        c1.verificarEmailDominioGoogle(c1.getEmail());
+        System.out.println(c1);
+        System.out.println("---------------------------------------------");
         
-        
-        Cliente c2 = new Cliente("Armando García");
+        Cliente c2 = new Cliente("Ari Santos");
         System.out.println(c2);
+        System.out.println("--------------------------------------------");
         double valorLocacaoLancamento = c2.CalcularLocação(5, true);
         System.out.println("O valor de locação do filme " + "Lançamento é: " + valorLocacaoLancamento);
-        
-        
+        c2.validarNome(c2.getNome());
+        System.out.println(c2);
+
     }
 }
